0a221c5d17d54205a5fb970465b8141ddfedaf4d
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-25 14:30:56 -0500

    began creating abstract player class

---
+++ BattleShip/Game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace BattleShip
+{
+    public class Game
+    {
+        // Member Variables
+        Player player1;
+        Player player2;
+        Grid grid;
+
+        // Constructors
+        public Game()
+        {
+            SetUpGame();
+            RunGame();
+        }
+
+        // Methods
+        public void SetUpGame() 
+        {
+            
+        }
+
+        public void RunGame()
+        {
+
+        }
+    }   
+}
---
+++ BattleShip/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BattleShip
+{
+    public abstract class Player
+    {
+        // Members
+        public string name;
+        bool isPlayer1;
+
+        // Constructors
+        public Player()
+        {
+            GetName(isPlayer1); 
+        }
+
+        // Methods
+        public void GetName(bool isPlayer1) {
+            string welcome;
+
+
+            welcome = isPlayer1 ? "Welcome Player 1. What is your name?" : "Welcome Player 2. What is your name?";
+            name = Console.ReadLine();
+        }
+    }
+}
