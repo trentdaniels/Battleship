7172934eb9036c7317a026541238ad5e520f49df
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-26 08:49:50 -0500

    refined method to create boards and return the board

--- BattleShip/Board.cs
+++ BattleShip/Board.cs
@@ -13,41 +13,50 @@ namespace BattleShip
         // Constructor
         public Board()
         {
-            targets = new List<int>{ };
+            targets = new List<int> { };
             Console.WriteLine(CreateBoard());
+
         }
 
         // Methods
-        public int[][] CreateBoard() 
+        public int[][] CreateBoard()
         {
-            
+
             int[][] board;
 
 
             Console.WriteLine("How many rows does the board have? Please choose a number (20 and above).");
             boardDimension = int.Parse(Console.ReadLine());
-
-            if (boardDimension >= 20) 
-            {
-                Console.WriteLine($"Each Player will have a board with and width and height of {boardDimension} units.");
-                board = new int[boardDimension][];
-                size = $"{boardDimension} x {boardDimension}";
-                for (int i = 0; i < board.Length; i++) 
-                {
-                    board[i] = new int[boardDimension];
-                }
-                Console.WriteLine(size);
-                Console.ReadLine();
-
-            }
-            else 
+            if (boardDimension < 20)
             {
                 Console.WriteLine("Your desired board size is too small. Please try again.");
                 CreateBoard();
 
             }
 
+            Console.WriteLine($"Each Player will have a board with and width and height of {boardDimension} units.");
+            board = new int[boardDimension][];
+
+            size = $"{boardDimension} x {boardDimension}";
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = new int[boardDimension];
+            }
+            Console.WriteLine(size);
+            Console.ReadLine();
+
+            foreach (int[] row in board)
+            {
+                Array.Clear(row, 0, row.Length);
+            }
+            return board;
 
         }
+
+
+
     }
+
+
 }
+
